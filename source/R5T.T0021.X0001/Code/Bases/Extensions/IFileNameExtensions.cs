68cc8e0296dcf0a4264928284bb4ca66bac798d7
David Coats <david@getsafetycone.com>
2021-09-28 15:13:49 -0700

    Add "Project Plan" IFileName value provider extension.

---
+++ source/R5T.T0021.X0001/Code/Bases/Extensions/IFileNameExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0021;
+using R5T.T0021.X0001;
+
+
+namespace System
+{
+    public static class IFileNameExtensions
+    {
+        public static string ProjectPlan(this IFileName _)
+        {
+            return FileNames.ProjectPlanText;
+        }
+    }
+}
---
+++ source/R5T.T0021.X0001/Code/Constants/FileNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.T0021.X0001
+{
+    public static class FileNames
+    {
+        public static string ProjectPlanText => "Project Plan.txt";
+    }
+}
